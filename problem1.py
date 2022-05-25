12d9b859b58ae097eaebf23c7e2af2dd3e235163
Jose Jimenez <josejimenez19855@gmail.com>
2022-05-25 16:17:48 -0500

    submit

--- problem1.py
+++ problem1.py
@@ -1,19 +1,31 @@
-flowerbed = [1,0,0,0,1]
-n = 2
-
-def solution(arr, n):
-    if n == 0:
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        self.sol(s)
+        
+    def validParenthesis(self, s, numStars):
+        map = {')':'(', '}':'{', ']':'['}
+        stack = []
+        for c in s:
+            if c in map:
+                if stack and map[c] != stack.pop():
+                    return False
+                elif numStars > 0 and stack:
+                    stack.pop()
+                    numStars -= 1
+            else:
+                stack.append(c)
         return True
-    elif n > 0:
-        for i in range(1,len(arr) - 1):
-            if arr[i] == 0 and arr[i - 1] != 1 and arr[i + 1] != 1:
-                arr[i] = 1
-                n -= 1
-        if n == 0:
-            return True
-        if n > 0:
-            return False
-                
-    return True
 
-print(solution(flowerbed, n))
+    def sol(self, s):
+        newString = []
+        numStars = 0
+        for c in s:
+            if c != '*':
+                numStars += 1
+                newString.append(c)
+
+        newString = ''.join(newString)
+        if self.validParenthesis(newString, numStars):
+            return True
+        else:
+            return False
