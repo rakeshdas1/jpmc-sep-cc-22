53f2fd72829b126fe7b85a73598d63675defa375
Tanjimul H. Bhuiyan <38303217+th3pi@users.noreply.github.com>
2022-05-25 15:46:35 -0500

    Create flowers.java

---
+++ flowers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n==0) return true;
+        if(flowerbed.length==1&&flowerbed[0]==0) {
+            n--;
+        }
+        if(n==0) return true;
+        for(int i=0;i<flowerbed.length;i++){
+            if(i==0){
+                if(flowerbed[i]==0 && flowerbed[i+1]==0){
+                    flowerbed[i]=1; n--;
+                }
+                
+            } else if (i==flowerbed.length-1){
+                
+                if(flowerbed[i-1]==0 && flowerbed[i]==0){
+                    flowerbed[i]=1; n--;
+                }
+                
+            }else{
+                if(flowerbed[i-1]==0 && flowerbed[i]==0 && flowerbed[i+1]==0){
+                    flowerbed[i]=1; n--;
+                }
+            }
+            
+            if(n==0) return true;
+            
+        }
+        return false;
+    }
+}
