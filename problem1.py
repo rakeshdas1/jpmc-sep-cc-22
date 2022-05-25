c87a15073faa4137631c3e5b8e33db22ee7a54f5
Jose Jimenez <josejimenez19855@gmail.com>
2022-05-25 16:47:20 -0500

    Problem 3

--- problem1.py
+++ problem1.py
@@ -1,31 +1,11 @@
 class Solution:
-    def checkValidString(self, s: str) -> bool:
-        self.sol(s)
-        
-    def validParenthesis(self, s, numStars):
-        map = {')':'(', '}':'{', ']':'['}
-        stack = []
-        for c in s:
-            if c in map:
-                if stack and map[c] != stack.pop():
-                    return False
-                elif numStars > 0 and stack:
-                    stack.pop()
-                    numStars -= 1
-            else:
-                stack.append(c)
-        return True
-
-    def sol(self, s):
-        newString = []
-        numStars = 0
-        for c in s:
-            if c != '*':
-                numStars += 1
-                newString.append(c)
-
-        newString = ''.join(newString)
-        if self.validParenthesis(newString, numStars):
-            return True
-        else:
-            return False
+    def countPairs(self, deliciousness: List[int]) -> int:
+        count = 0
+        for i in range(len(deliciousness)):
+            for j in range(i+1, len(deliciousness)):
+                d = deliciousness[i] + deliciousness[j]
+                while d % 2 == 0:
+                    d //= 2
+                if d == 1:
+                    count += 1 
+        return count % (10 ** 9 + 7)
